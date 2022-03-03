afaa14f098ccab03d779a9c0d31f770852072372
Bob Sira <Bob.Sira@microsoft.com>
2022-03-03 21:10:50 +0000

    Maximize Distance to Closest Person

--- src/arraystrings/code.java
+++ src/arraystrings/code.java
@@ -1,5 +1,7 @@
 package arraystrings;
 
+import java.util.Arrays;
+
 public class code {
 
     static String uncompress(String s){
@@ -34,5 +36,29 @@ public class code {
             }
         }
         return result.toString();
+    }
+
+    static int maxDistToClosest(int[] seats){
+        int N = seats.length;
+        int [] left = new int[N];
+        int [] right = new int[N];
+        Arrays.fill(left, N);
+        Arrays.fill(right, N);
+
+        for (int i = 0; i < N; i++){
+            if (seats[i] == 1) left[i] = 0;
+            else if (i > 0) left[i] = left[i - 1] + 1;
+        }
+
+        for (int i = N - 1; i >= 0; i--){
+            if (seats[i] == 1) right[i] = 0;
+            else if (i < N - 1) right[i] = right[i + 1] + 1;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < N; i++){
+            if (seats[i] == 0) answer = Math.max(answer, Math.min(left[i], right[i]));
+        }
+        return answer;
     }
 }
