85a347dfc16a417f07b6c01cb2da159548574cbe
Bob Sira <Bob.Sira@microsoft.com>
2022-03-05 21:38:08 +0000

    Pascal's Triangle

--- src/arraystrings/code.java
+++ src/arraystrings/code.java
@@ -120,4 +120,35 @@ public class code {
         }
         return results;
     }
+
+    static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        triangle.add(firstRow);
+        for (int i = 1; i < numRows; i++){
+            List<Integer> previousRow = triangle.get(i - 1);
+            List<Integer> currentRow = new ArrayList<>();
+            // populate the columns in the current row
+            currentRow.add(1);
+            for (int j = 1; j < i; j++){
+                currentRow.add(previousRow.get(j - 1) + previousRow.get(j));
+            }
+            currentRow.add(1);
+            triangle.add(currentRow);
+        }
+        return  triangle;
+    }
+
+    static List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>(rowIndex + 1);
+        row.add(1);
+        for (int i = 0; i < rowIndex; i++){
+            for (int j = i; j > 0; j--){
+                row.set(j, row.get(j - 1) + row.get(j));
+            }
+            row.add(1);
+        }
+        return row;
+    }
 }
