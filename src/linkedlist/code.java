535c49caba1531ecca4e84b2e3a05de5df4760f2
Bob Sira <Bob.Sira@microsoft.com>
2022-03-10 12:47:18 +0000

    Merge Two Lists Recursive

--- src/linkedlist/code.java
+++ src/linkedlist/code.java
@@ -290,6 +290,17 @@ public class code {
         if(current2 != null) tail.next = current2;
         return sentinel.next;
     }
+    public ListNode mergeTwoListsRecursive(ListNode list1, ListNode list2) {
+         if (list1 == null) return list2;
+         if (list2 == null) return list1;
+         if (list1.val < list2.val){
+             list1.next = mergeTwoListsRecursive(list1.next, list2);
+             return list1;
+         } else {
+             list2.next = mergeTwoListsRecursive(list1, list2.next);
+             return list2;
+         }
+    }
 
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> heap = new PriorityQueue<>( (Comparator<ListNode>) (o1, o2) -> o1.val - o2.val);
